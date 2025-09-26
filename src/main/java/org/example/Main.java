5318d71c14b3e3c4806efbc022cfeace9e8a2007
nadineirakoze5 <nirakoze72@gmail.com>
2025-09-26 13:02:38 +0200

    Doing some changes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,26 @@
 package org.example;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main extends Application {
+
+ public class Main extends Application {
     public static void main(String[] args) {
+      Application.launch(Main.class, args);
 
     }
 
     @Override
     public void start(Stage stage) throws Exception {
+        stage.setTitle("Text Processing Tool");
+        GridPane gridPane = new GridPane();
+        gridPane.setHgap(20);
+        gridPane.setVgap(20);
+        gridPane.add();
+        stage.setScene(new Scene(gridPane));
+
 
     }
 }
